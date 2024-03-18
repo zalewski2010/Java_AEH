1af2ef4ab1783b7c4c29afb404b5cc2153b542fb
Krzysztof Zalewski <zalewski2010@gmail.com>
2024-03-18 11:17:56 +0100

    2024-03-18 zad2 i zad 3

--- excercises/zadaniaLab.java
+++ excercises/zadaniaLab.java
@@ -10,6 +10,16 @@ public class zadaniaLab {
         System.out.println(multipl(3,3)+ " to wynik mnożenia zad1");
         System.out.println(divid(12,4) + " to wynik dzielenia zad1");
         
+        //wypisanie zad2 - avg tablicy
+        int[] integerNumbers = {1, 2, 3, 4, 6, 14};
+        System.out.println("średnia z tablicy w zad2 to: " + calcAvg(integerNumbers));
+
+        // Wypisanie zad3 parzysta czy nieparzysta liczba
+        int someNumb = 7; //liczba do sprawdzenia
+        System.out.println("Liczba " + someNumb + " jest " + evenOdd(someNumb) + " (even or odd? zad3)");
+
+    
+        
     }
     // zadanie 1 = Wyświetl wynik dodawania, odejmowania, mnożenia oraz dzielenia 2 wartości.
     // - jest to wersja bez wyboru rodzaju operacji, i bez inputa dla użytkownika
@@ -25,19 +35,33 @@ public class zadaniaLab {
                 int sum = a * b;
                 return sum; }
 
-    public static int divid(int a , int b) {
-                    int sum = a / b;
+    public static double divid(int a , int b) {
+                    double sum = a / b;
                     return sum; }
-
     
-
+    // Zadanie 2. Stwórz tablicę liczb całkowitych a następnie oblicz średnią wartość.
+    public static double calcAvg(int[] array) {
+        if (array == null || array.length == 0) {
+            return 0; // Zwraca 0, jeśli tablica jest pusta lub null
+        }
+        double sum = 0;
+        for (int number : array) {
+            sum += number; // Sumuje wszystkie liczby w tablicy
+        }
+        return sum / array.length; // Dzieli sumę przez liczbę elementów, aby uzyskać średnią
+    }
     
+    // Zadanie 3 Wyświetl, czy dana liczba jest parzysta czy nieparzysta.
+    public static String evenOdd(int number) {
+        return (number % 2 == 0) ? "even" : "odd";
+    }
+
+
 }
 
 
 
-//2) Stwórz tablicę liczb całkowitych a następnie oblicz średnią wartość.
-//3) Wyświetl, czy dana liczba jest parzysta czy nieparzysta.
+
 //4) Napisz program obliczający wskaźnik masy ciała (BMI).
 //5) Napisz program, który dla tablicy wyników z kilkoma ocenami obliczy średnią ocen i określi odpowiedni stopień zaliczenia (np. "A", "B", "C" itp.).
 //6) Napisz program, który sprawdza, czy dane słowo jest palindromem.
